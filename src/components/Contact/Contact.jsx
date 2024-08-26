1304696bb7b9f65c0129c46bf8647e97d926034a
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-08-26 18:00:36 +0200

    contact.js correct

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,25 @@
 import css from './Contact.module.css';
-import { ImPhone, ImUser  } from "react-icons/im";
+import { ImPhone, ImUser } from "react-icons/im";
+import { deleteContact } from '../../redux/contactsSlice';
+import { useDispatch } from 'react-redux';
 
 
+const Contact = ({ id, name, number }) => {
 
-const Contact = ({ id, name, number, deleteContacts }) => (
+  const dispatch = useDispatch();
+  
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
   <li className={css.contact}>
     <p> < ImUser size="16"/> {name}</p>
     <p> < ImPhone size="16"/> {number}</p>
-    <button className={css.deleteBtn} onClick={() => deleteContacts(id)}>Delete</button>
+    <button className={css.deleteBtn} onClick={handleDelete}>Delete</button>
   </li>
-);
+  );
+};
 
 export default Contact;
 
